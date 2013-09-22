e66115beb5acc1b7f38e388caf76303d80d7be70
raeffu <raeffu@raeffu.ch>
2013-09-22 11:33:03 +0200

    correct withespace, use jre 1.6

--- src/ch01/hello/HelloPrinter.java
+++ src/ch01/hello/HelloPrinter.java
@@ -2,8 +2,8 @@ package ch01.hello;
 
 public class HelloPrinter {
 
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-	}
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
 
 }
