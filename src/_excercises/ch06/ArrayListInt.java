8aee1aa5e4a25362621b8537f66c16c1afc5a08e
raeffu <raeffu@raeffu.ch>
2013-10-25 14:35:31 +0200

    starting with own ArrayList implementation

---
+++ src/_excercises/ch06/ArrayListInt.java
@@ -0,0 +1,84 @@
+package _excercises.ch06;
+
+import java.util.Arrays;
+
+public class ArrayListInt {
+
+  private int[] array;
+  private int size;
+
+  /*
+   * Constructor
+   */
+  public ArrayListInt() {
+    array = new int[100];
+    size = 0;
+  }
+
+  public ArrayListInt(int initialLength) {
+    array = new int[initialLength];
+    size = 0; // next free space
+  }
+
+  /*
+   * Methods
+   */
+  public int get(int index) {
+    return array[index];
+  }
+
+  public void set(int index, int element) {
+    array[index] = element;
+  }
+
+  public void add(int element) {
+    if (size >= array.length) {
+      this.grow();
+    }
+    
+    array[size] = element;
+    size++;
+  }
+
+  public void insert(int index, int element) {
+    for (int i = size; i > index; i--) {
+      if (i == size) {
+        this.add(array[i-1]);
+      }
+      else {
+        array[i] = array[i-1];
+      }
+    }
+  }
+
+  public void remove(int index) {
+    if (index < size) {
+      array[index] = array[size-1];
+      size--;
+    }
+    
+    if (array.length/size >= 2) {
+      this.shrink();
+    }
+  }
+
+  public String toString() {
+    String values = "";
+    for (int element : array) {
+      values += element + ", ";
+    }
+    
+    // remove last ", "
+    values = values.substring(0, values.length()-2);
+    
+    return "[" + values + "]";
+  }
+
+  private void grow() {
+    array = Arrays.copyOf(array, array.length * 2);
+  }
+
+  private void shrink() {
+    array = Arrays.copyOf(array, array.length / 2);
+  }
+}
---
+++ src/_excercises/ch06/ArrayListTest.java
@@ -0,0 +1,11 @@
+package _excercises.ch06;
+
+public class ArrayListTest {
+
+  public static void main(String[] args) {
+    ArrayListInt test = new ArrayListInt(-1);
+    
+    System.out.println(test);
+  }
+
+}
